92a5fb9c9991d5f464a2ff5d0e08b8abc6937b80
Proshanto <proshantolal3@gmail.com>
2023-06-30 17:51:46 +0600

    divides route for summary and stats

--- server/src/modules/summary/summary.routes.js
+++ server/src/modules/summary/summary.routes.js
@@ -1,8 +1,11 @@
 const validate = require('../core/middlewares/validate');
 const { fetchSummarySchema } = require('./summary.schema');
-const { fetchUserSummary } = require('./summary.controller');
+const { fetchUserProblemsSolved, fetchUserSummary } = require('./summary.controller');
 
 module.exports = (app) => {
     app.route('/summary')
         .post(validate(fetchSummarySchema), fetchUserSummary)
+    
+    app.route('/stats')
+        .post(validate(fetchSummarySchema), fetchUserProblemsSolved)
 }
