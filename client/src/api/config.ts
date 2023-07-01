081543b53b17d960d81312072f8ec8bd7bcf62ad
Proshanto <proshantolal3@gmail.com>
2023-07-01 22:18:10 +0600

    changes server config for client

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,6 +1,7 @@
 import { QueryClient } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient();
-const BASE_URL = 'http://localhost:3000';
+// Free production deploy server
+const BASE_URL = "https://quaint-lamb-jumper.cyclic.app";
 export const STATS_URL = `${BASE_URL}/stats`;
 export const SUMMARY_URL = `${BASE_URL}/summary`;
