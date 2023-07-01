a0629d2d3eac8dc52979bbf6c473a9093da6eba3
Proshanto <proshantolal3@gmail.com>
2023-07-01 23:00:41 +0600

    testing config files no 2

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -4,6 +4,6 @@ export const queryClient = new QueryClient();
 // for local build use http://localhost:3000
 const BASE_URL = import.meta.env.VITE_REACT_APP_SERVER_URL;
 console.log(BASE_URL)
-export const STATS_URL = `${BASE_URL}/stats`;
-console.log(STATS_URL)
-export const SUMMARY_URL = `${BASE_URL}/summary`;
+export const STATS_URL = `${import.meta.env.VITE_REACT_APP_SERVER_URL}/stats`;
+export const SUMMARY_URL = `${import.meta.env.VITE_REACT_APP_SERVER_URL}/summary`;
+console.log(SUMMARY_URL)
