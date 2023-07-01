de620d0c602650214ff70f91d6097370cd77c62b
Proshanto <proshantolal3@gmail.com>
2023-07-01 22:25:21 +0600

    server location shifted to .env file

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -2,6 +2,6 @@ import { QueryClient } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient();
 // for local build use http://localhost:3000
-const BASE_URL = 'https://quaint-lamb-jumper.cyclic.app';
+const BASE_URL = process.env.SERVER;
 export const STATS_URL = `${BASE_URL}/stats`;
 export const SUMMARY_URL = `${BASE_URL}/summary`;
