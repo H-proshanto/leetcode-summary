3fd4d973d1700a321ffe81b51cb179f08e4b781a
Proshanto <proshantolal3@gmail.com>
2023-07-01 23:03:25 +0600

    updates config for client

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -2,8 +2,6 @@ import { QueryClient } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient();
 // for local build use http://localhost:3000
-const BASE_URL = import.meta.env.VITE_REACT_APP_SERVER_URL;
-console.log(BASE_URL)
-export const STATS_URL = `${import.meta.env.VITE_REACT_APP_SERVER_URL}/stats`;
-export const SUMMARY_URL = `${import.meta.env.VITE_REACT_APP_SERVER_URL}/summary`;
-console.log(SUMMARY_URL)
+const BASE_URL = 'https://quaint-lamb-jumper.cyclic.app';
+export const STATS_URL = `${BASE_URL}/stats`;
+export const SUMMARY_URL = `${BASE_URL}/summary`;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,7 @@
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': dotenv.config().parsed
-  }
 })
