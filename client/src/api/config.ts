764fb601ef41854c99f15c7174c87303bef6f92b
Proshanto <proshantolal3@gmail.com>
2023-07-01 22:52:13 +0600

    testing env for vercel

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -2,6 +2,6 @@ import { QueryClient } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient();
 // for local build use http://localhost:3000
-const BASE_URL = process.env.REACT_APP_SERVER_URL;
+const BASE_URL = import.meta.env.REACT_APP_SERVER_URL;
 export const STATS_URL = `${BASE_URL}/stats`;
 export const SUMMARY_URL = `${BASE_URL}/summary`;
