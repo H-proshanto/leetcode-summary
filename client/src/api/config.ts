dacbe0d77f210fc99e725903f8aa0b28bd20c65b
Proshanto <proshantolal3@gmail.com>
2023-07-01 22:59:06 +0600

    testing config files

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -3,5 +3,7 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient();
 // for local build use http://localhost:3000
 const BASE_URL = import.meta.env.VITE_REACT_APP_SERVER_URL;
+console.log(BASE_URL)
 export const STATS_URL = `${BASE_URL}/stats`;
+console.log(STATS_URL)
 export const SUMMARY_URL = `${BASE_URL}/summary`;
